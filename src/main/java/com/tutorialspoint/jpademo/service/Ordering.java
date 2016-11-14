8e1345bf83694c52af28d0d9a885df3dfbf029e4
maxline <sergml@mail.ru>
2016-11-14 21:28:23 +0200

    v02_2. add Ordering class.
    Example JPQL with 'order by' clause.

--- src/main/java/com/tutorialspoint/jpademo/service/Ordering.java
+++ src/main/java/com/tutorialspoint/jpademo/service/Ordering.java
@@ -11,28 +11,18 @@ import java.util.List;
 /**
  * @author Sergey Mikhluk.
  */
-public class BetweenAndLikeFunctions {
+public class Ordering {
     public static void main(String[] args) {
         EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
         EntityManager entityManager = emFactory.createEntityManager();
 
         //Between
         Query query = entityManager.createQuery("select e " + "from Employee e "
-                + "where e.salary " + "between 100 and 5000");
+        + "order by e.eName DESC");
 
-        List<Employee> list = (List<Employee>) query.getResultList();
+        List<Employee> list = (List<Employee>)query.getResultList();
 
-        for (Employee e : list) {
-            System.out.println(e);
-        }
-
-        //Like
-        Query query1 = entityManager.createQuery("select e " + "from Employee e "
-                + "where e.eName like 'I%'");
-
-        List<Employee> list1 = (List<Employee>) query1.getResultList();
-
-        for(Employee e: list1){
+        for (Employee e: list){
             System.out.println(e);
         }
 
