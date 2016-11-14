360f8a4351952b1a1e4f130b68d25fb1e5a3edd2
maxline <sergml@mail.ru>
2016-11-14 20:48:00 +0200

    v01_2. add UpdateEmployee class. works.
    Example of updating employee DB record by JPA usage.

---
+++ src/main/java/com/tutorialspoint/UpdateEmployee.java
@@ -0,0 +1,30 @@
+package com.tutorialspoint;
+
+import com.tutorialspoint.jpademo.domain.Employee;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * @author Sergey Mikhluk.
+ */
+public class UpdateEmployee {
+    public static void main(String[] args) {
+        EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
+        EntityManager entityManager = emFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+        Employee employee = entityManager.find(Employee.class, 1201);
+
+        //before update
+        System.out.println(employee);
+        employee.setSalary(4500);
+        entityManager.getTransaction().commit();
+
+        //after update
+        System.out.println(employee);
+        entityManager.close();
+        emFactory.close();
+    }
+}
